b6f0712f51a66e6c9a1caf7244921208bcb4cf48
KumPong <ananyod.cha@spumail.net>
2026-07-06 22:24:51 +0700

    Resolve first conflict: keep my sidebars

--- Frontend/src/layouts/customerLayout.jsx
+++ Frontend/src/layouts/customerLayout.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar";
+import Footer from "../components/footer";
 
 export default function CustomerLayout() {
     return (
         <div>
             <Navbar />
-            <main className="p-4">
+
+            <main className="flex-1 p-4">
                 <Outlet />
             </main>
+
+            <Footer/>
         </div>
     )
 }
