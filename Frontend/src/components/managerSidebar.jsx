6afe873de941b4433bdae2bf074841b0d659a2e7
SaBo4K <notinw001@gmail.com>
2026-07-13 08:11:30 +0700

    chore: remove unused print:hidden class from managerSidebar
    Leftover from the print/export feature that was removed from report.jsx.

--- Frontend/src/components/managerSidebar.jsx
+++ Frontend/src/components/managerSidebar.jsx
@@ -33,7 +33,7 @@ function ManagerSidebar() {
     ]
 
     return(
-        <aside className="w-64 h-screen bg-other flex flex-col border-r border-gray-200 print:hidden">
+        <aside className="w-64 h-screen bg-other flex flex-col border-r border-gray-200">
             {/* Header & Logo */}
             <div className="p-6 flex items-center gap-3">
                 <NavLink className="shrink-0">
